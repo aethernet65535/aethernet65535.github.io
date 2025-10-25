ea26b3de272bd11f88b3731c4a072096b5dfea3b
aethernet <aethernet65535@gmail.com>
2025-10-25 17:54:28 +0800

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,9 +27,9 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://aethernet65535.github.io",
+	site: "https://aethernet65535.github.io/",
 
-	base: "/",
+	base: "/aethernet65535.github.io",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
