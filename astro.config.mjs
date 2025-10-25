bf0f1b7c54cd7ef82b5f89f58fb2c5ce1b0fbfb0
aethernet <aethernet65535@gmail.com>
2025-10-25 18:16:45 +0800

    delete base

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,8 +28,6 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://aethernet65535.github.io",
-
-	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
