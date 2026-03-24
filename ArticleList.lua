1d15105023a0f01a93ad4f6414754a06e0159dff
Liew Rui Yan <aethernet65535@gmail.com>
2026-03-24 23:13:54 +0800

    boot: engine from ziyao233
    Signed-off-by: Liew Rui Yan <aethernet65535@gmail.com>

---
+++ ArticleList.lua
@@ -0,0 +1,133 @@
+{
+	{
+		name	= "about",
+		title	= "About",
+		date	= "2022-01-30",
+		change	= "2023-11-29",
+	},
+	{
+		name	= "links",
+		title	= "友情链接",
+		date	= "2022-01-30",
+		change	= "2024-03-12",
+	},
+	{
+		name	= "rpi4b-system-reset",
+		title	= "树莓派4B 系统重装",
+		date	= "2022-03-13",
+	},
+	{
+		name	= "my-2021",
+		title	= "我的2021",
+		date	= "2022-04-22",
+	},
+	{
+		name	= "MathJax_Test",
+		title	= "MathJax 测试",
+		date	= "2022-07-25",
+	},
+	{
+		name	= "Sum_1",
+		title	= "和式-成套方法",
+		date	= "2022-07-25",
+	},
+	{
+		name	= "Limit_L_Hospital",
+		title	= "微积分-洛必达法则",
+		date	= "2022-07-26",
+	},
+	{
+		name	= "Sum_2",
+		title	= "和式-技巧",
+		date	= "2022-07-27",
+	},
+	{
+		name	= "Luogu_P1403",
+		title	= "洛谷 P1403",
+		date	= "2022-08-06",
+	},
+	{
+		name	= "DP_1",
+		title	= "动态规划初步",
+		date	= "2022-08-12",
+	},
+	{
+		name	= "Alpine_Packaging",
+		title	= "Alpine Linux 打包",
+		date	= "2022-08-14",
+	},
+	{
+		name	= "CSP_S1_2021",
+		title	= "CSP-S1 题目",
+		date	= "2022-08-21",
+	},
+	{
+		name	= "Fleeding",
+		title	= "逃离",
+		date	= "2022-10-04",
+	},
+	{
+		name	= "Luogu_P1880",
+		title	= "Luogu P1880",
+		date	= "2022-10-27",
+	},
+	{
+		name	= "asmbf",
+		title	= "asmbf: 汇编 brainfuck 解释器的实现",
+		date	= "2022-12-22",
+	},
+	{
+		name	= "2022",
+		title	= "2022",
+		date	= "2023-01-03",
+	},
+	{
+		name	= "riscv_qemu_pmp",
+		title	= "记录 RISC-V QEMU 的一个坑",
+		date	= "2023-04-27",
+	},
+	{
+		name	= "CQ_Satelite",
+		title	= "在卫星上喊 CQ",
+		date	= "2023-11-29",
+	},
+	{
+		name	= "cdda_guide",
+		title	= "Cataclysm-DDA 入门",
+		date	= "2023-12-31",
+	},
+--[[
+	I'm a pigeon.....
+	{
+		name	= "gcc_development_on_super_slow_arch",
+		title	= "优雅地在超（级慢动作计）算（机）调试 gcc",
+		date	= "2024-10-19",
+	},
+	{
+		name	= "llvm_cross_and_bootstrap",
+		title	= "LLVM 交叉编译与 bootstrap: riscv64 实践",
+		date	= "2024-10-19",
+	},
+--]]
+	{
+		name	= "linux_boot_protocol",
+		title	= "How Linux Boots: the Linux Boot Protocol" ..
+			  " (RISC platforms)",
+		date	= "2024-12-28",
+	},
+	{
+		name	= "my_2024",
+		title	= "我的 2024",
+		date	= "2025-02-01",
+	},
+	{
+		name	= "string_bsd_in_infradead_mail",
+		title	= "No, lists.infraded.org doesn't run on BSD",
+		date	= "2025-09-23",
+	},
+	{
+		name	= "my_2025",
+		title	= "半夜的年终总结：我的 2025",
+		date	= "2026-02-16",
+	},
+}
