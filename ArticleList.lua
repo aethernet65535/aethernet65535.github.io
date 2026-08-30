2bc9efcb80fdd0256c5a16d27abf45cb5f6bce9c
Liew Rui Yan <aethernet65535@gmail.com>
2026-08-30 14:56:48 +0800

    update
    Signed-off-by: Liew Rui Yan <aethernet65535@gmail.com>

--- ArticleList.lua
+++ ArticleList.lua
@@ -1,9 +1,9 @@
 {
     {
-        name	= "cn-about-me",
-        title	= "关于我",
-        date	= "2026-03-24",
-        change	= "2026-03-25",
+        name    = "cn-about-me",
+        title   = "关于我",
+        date    = "2026-03-24",
+        change  = "2026-03-25",
     },
     {
         name    = "cn-virtme-ng",
@@ -15,6 +15,6 @@
         name    = "todo",
         title   = "TODO List",
         date    = "2026-08-30",
-        change    = "2026-08-30",
+        change  = "2026-08-30",
     },
 }
